4b62d42a8b33816a87336b78c109dde5e55f5268
Zahidashahid <71212009+Zahidashahid@users.noreply.github.com>
2021-07-30 00:07:04 +0500

    Update
    Name
    projects

--- js/app.js
+++ js/app.js
@@ -22,12 +22,12 @@ var waypoint = new Waypoint({
   handler: function() {
 
     var p = document.querySelectorAll('.progress-bar');
-    p[0].setAttribute("style", "width:100%;transition:1s all;");
-    p[1].setAttribute("style", "width:95%;transition:1.5s all;");
-    p[2].setAttribute("style", "width:85%;transition:1.7s all;");
-    p[3].setAttribute("style", "width:99%;transition:2s all;");
+    p[0].setAttribute("style", "width:90%;transition:1s all;");
+    p[1].setAttribute("style", "width:90%;transition:1.5s all;");
+    p[2].setAttribute("style", "width:75%;transition:1.7s all;");
+    p[3].setAttribute("style", "width:90%;transition:2s all;");
     p[4].setAttribute("style", "width:85%;transition:2.3s all;");
-    p[5].setAttribute("style", "width:95%;transition:2.5s all;");
+    p[5].setAttribute("style", "width:75%;transition:2.5s all;");
 
 
   },
@@ -88,7 +88,7 @@ var filterizd = $('.filter-container').filterizr({
 
 
   var typed = new Typed(".element", {
-  strings: ["Abdul Rahim", "a Developer","a DevOps Enthusiast"],
+  strings: ["Zahida Shahid", "a Developer","Software Engineer","Game Development Enthusiast"],
   smartBackspace: true,
    typeSpeed: 100,
    backSpeed: 100,
